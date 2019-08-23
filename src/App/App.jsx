62d9406f6a373f710853dd0c66da49da9528f1cd
john <johnwee35@gmail.com>
2019-08-23 21:19:54 +0800

    add app.js and app.css component

---
+++ src/App/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import moment from "moment";
+import "./App.css";
+
+const App = () => {
+    console.log(moment.weekdaysShort());
+    return (
+        <div className="app">
+            <div className="app__calandar">
+                
+            </div>
+        </div>
+    )
+}
+
+export default App
