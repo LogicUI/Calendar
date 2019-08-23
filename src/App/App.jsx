89de85eef6bd96cdf463d0afca2e71c4fa973fb8
john <johnwee35@gmail.com>
2019-08-23 21:38:18 +0800

    add date selector

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./App.css";
 import Header from "../Header/Header";
 
 const App = () => {
     return (
         <div className="app">
-            
+            <label className="app__dateSelector">Select A date</label>
             <div className="app__calandar">
                 <Header />
             </div>
