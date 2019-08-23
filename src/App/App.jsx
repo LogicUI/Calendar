7e5985f0131c99f87ee6588b19d558fce515cfa3
john <johnwee35@gmail.com>
2019-08-23 22:19:20 +0800

    add moment format for month and year

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,17 @@
 import React,{useState} from 'react'
 import "./App.css";
 import Header from "../Header/Header";
+import moment from "moment";
+import MonthYear from "../MonthYear/MonthYear";
 
 const App = () => {
+    const today = moment(new Date()).format("MMMM");
+    const year = moment(new Date()).format("YYYY");
     return (
         <div className="app">
             <label className="app__dateSelector">Select A date</label>
             <div className="app__calandar">
+                <MonthYear month={today} year={year} />
                 <Header />
             </div>
         </div>
