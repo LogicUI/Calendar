26f08ad991ac730b74ed3e1c385f8bc33750dd47
john <johnwee35@gmail.com>
2019-08-25 01:19:54 +0800

    remove month and year from calendar cell

--- src/Calendar/CalendarCell/CalendarCell.jsx
+++ src/Calendar/CalendarCell/CalendarCell.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./CalendarCell.css";
 
-const CalendarCell = ({date, change}) => {
+const CalendarCell = ({date, change, month, year}) => {
     return (
-        <div className={date ? "calenderCell"  : ""} onClick={() => change(date)}>
+        <div className={date ? "calenderCell"  : ""} onClick={() => change(date,month,year)}>
             {date}
         </div>
     )
