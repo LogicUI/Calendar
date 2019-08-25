fb3c25cb737d1f93cc977e9d6864394c50be1f2a
john <johnwee35@gmail.com>
2019-08-25 15:47:12 +0800

    include ref for togglecalendar custom hook

--- src/CustomHooks/CalendarHooks/useToggleCalendar.js
+++ src/CustomHooks/CalendarHooks/useToggleCalendar.js
@@ -1,20 +1,20 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect , useRef} from 'react';
 
 const useToggleCalendar = () => {
-  const [toggle, toggleHidden] = useState(false);
+  const [toggle, showCalendar] = useState(false);
+   const calendarRef = useRef();
 
    const toggleHiddenCalendar = (event) => {
-        if (event.target.className === "app") {
-            toggleHidden(false)
-        }
+    if(calendarRef.current && !calendarRef.current.contains(event.target)){
+        showCalendar(false);
+    }
    }
-
    useEffect(() => {
         document.addEventListener("mousedown", toggleHiddenCalendar );
         return () => document.removeEventListener("mousedown", toggleHiddenCalendar);
     })
 
-   return [toggle , toggleHidden];
+   return [toggle , showCalendar ,calendarRef];
 }
 
 export default useToggleCalendar;
