0c97fab759fba6bf5e65348419c417bc7e82d0cf
john <johnwee35@gmail.com>
2019-08-25 15:46:30 +0800

    include proptype validator for calendarController

--- src/Calendar/CalendarController/CalendarController.jsx
+++ src/Calendar/CalendarController/CalendarController.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import "./CalendarController.css";
 import { ReactComponent as LeftArrow } from "../../Svg/left-arrow.svg";
 import { ReactComponent as RightArrow } from "../../Svg/right-arrow.svg";
+import PropTypes from 'prop-types';
+
+
 const CalendarController = ({month ,year , increase , decrease}) => {
     return (
         <div className="calendarController ">
@@ -9,13 +12,26 @@ const CalendarController = ({month ,year , increase , decrease}) => {
             <div className="calendarButtons">
                 <LeftArrow width="20px" height="20px" className="calendarButtons__svgButton"
                 onClick={decrease}
+                data-testid="leftArrow"
                 />
                 <RightArrow width="20px" height="20px" className="calendarButtons__svgButton"
                 onClick={increase}
+                data-testid="rightArrow"
                 />
             </div>
         </div>
     )
 }
+
+CalendarController.propTypes = {
+    month:PropTypes.string.isRequired,
+    year: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ]).isRequired,
+    increase: PropTypes.func.isRequired,
+    decrease: PropTypes.func.isRequired
+}
+
 
 export default CalendarController
