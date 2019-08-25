25643557fd3f8d4fb34fd2258a076b6daab7e676
john <johnwee35@gmail.com>
2019-08-25 15:45:43 +0800

    rename calandarpicker to calendarmonth in app component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./App.css";
-import CalendarPicker from "../Calendar/CalendarMain/CalendarMain";
+import CalendarMain from "../Calendar/CalendarMain/CalendarMain";
 import moment from "moment";
 
 const App = () => {
     return (
         <div className="app" >
             <h1>Calendar App</h1>
-            <CalendarPicker month={moment().format("M")} />
+            <CalendarMain month={moment().format("M")} />
         </div>
     )
 }
