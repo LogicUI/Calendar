e18f1dcfefaa96eff601484ef3f67f9474cd1da7
john <johnwee35@gmail.com>
2019-08-25 15:47:40 +0800

    include calendarRef for calendarmain to exit outside the calendar component

--- src/Calendar/CalendarMain/CalendarMain.jsx
+++ src/Calendar/CalendarMain/CalendarMain.jsx
@@ -1,4 +1,4 @@
-import React , { Fragment} from 'react'
+import React , { Fragment } from 'react'
 import "./CalendarMain.css"
 import CalendarDayName from "../CalendarDayName/CalendarDayName";
 import CalendarController from "../CalendarController/CalendarController";
@@ -6,29 +6,39 @@ import CalendarDateDays from "../CalendarDateDays/CalendarDateDays";
 import useToggleCalendar from "../../CustomHooks/CalendarHooks/useToggleCalendar";
 import useLabelDate from "../../CustomHooks/CalendarHooks/useLabelDate";
 import useCalendarHook from "../../CustomHooks/CalendarHooks/useCalendarHook";
-
+import PropTypes from 'prop-types';
 
 
 const CalendarMain = ({month}) => {
 
     const [date, decreaseMonth, increaseMonth] = useCalendarHook(month);
 
-    const [toggle, toggleHidden] = useToggleCalendar();
+    const [toggle, showCalendar, calendarRef] = useToggleCalendar();
 
     const [labelDate, changeLabelDate] = useLabelDate(date.month,date.year);
 
+
+
+
     return (
     <Fragment>
-       < label onClick = {() => toggleHidden(true)} className = "dateSelectorLabel" >
+       < label onClick = {() => showCalendar(true)} className = "dateSelectorLabel" >
          { labelDate? labelDate: "Select A Date" }
       </label >
-     {toggle && <div className="calendarMain">
+     {toggle && <div className="calendarMain" ref={calendarRef}>
             <CalendarController month={date.month} year={date.year} increase={increaseMonth} decrease={decreaseMonth} />
             <CalendarDayName />
            <CalendarDateDays firstDay={date.firstDay} daysInMonth={date.daysInMonth} change={changeLabelDate} />
     </div> }
    </Fragment>
     )
+}
+
+CalendarMain.propTypes = {
+    month: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ]).isRequired
 }
 
 export default CalendarMain
